ec602fb2a836cf22d5ec3b277afef8d6d048dd17
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2020-09-15 02:47:32 +0200

    Initial commit

---
+++ ProcessUtil/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ProcessUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.endprocess = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SelectProcessEnd = new MetroFramework.Controls.MetroComboBox();
+            this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.SelectProcessDump = new System.Windows.Forms.ComboBox();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.AllowDrop = true;
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage3);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroTabControl1.Location = new System.Drawing.Point(20, 60);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(760, 370);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.endprocess);
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.Controls.Add(this.SelectProcessEnd);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(752, 331);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Process Terminator";
+            this.metroTabPage1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            // 
+            // endprocess
+            // 
+            this.endprocess.Location = new System.Drawing.Point(24, 130);
+            this.endprocess.Name = "endprocess";
+            this.endprocess.Size = new System.Drawing.Size(157, 39);
+            this.endprocess.TabIndex = 4;
+            this.endprocess.Text = "End Process";
+            this.endprocess.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.endprocess.Click += new System.EventHandler(this.endprocess_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 44);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(91, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Select Process";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // SelectProcessEnd
+            // 
+            this.SelectProcessEnd.FormattingEnabled = true;
+            this.SelectProcessEnd.ItemHeight = 23;
+            this.SelectProcessEnd.Location = new System.Drawing.Point(3, 75);
+            this.SelectProcessEnd.Name = "SelectProcessEnd";
+            this.SelectProcessEnd.Size = new System.Drawing.Size(218, 29);
+            this.SelectProcessEnd.TabIndex = 2;
+            this.SelectProcessEnd.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.SelectProcessEnd.Click += new System.EventHandler(this.SelectProcessEnd_Click);
+            // 
+            // metroTabPage3
+            // 
+            this.metroTabPage3.Controls.Add(this.metroLabel2);
+            this.metroTabPage3.Controls.Add(this.SelectProcessDump);
+            this.metroTabPage3.HorizontalScrollbarBarColor = true;
+            this.metroTabPage3.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage3.Name = "metroTabPage3";
+            this.metroTabPage3.Size = new System.Drawing.Size(752, 331);
+            this.metroTabPage3.TabIndex = 2;
+            this.metroTabPage3.Text = "Process Dumper";
+            this.metroTabPage3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage3.VerticalScrollbarBarColor = true;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(-4477, 62);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(91, 19);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Select Process";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // SelectProcessDump
+            // 
+            this.SelectProcessDump.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SelectProcessDump.FormattingEnabled = true;
+            this.SelectProcessDump.Location = new System.Drawing.Point(3, 97);
+            this.SelectProcessDump.Name = "SelectProcessDump";
+            this.SelectProcessDump.Size = new System.Drawing.Size(163, 21);
+            this.SelectProcessDump.TabIndex = 2;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(752, 331);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Injector";
+            this.metroTabPage2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroTabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Process Utilities | X-ray#7777";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage3.ResumeLayout(false);
+            this.metroTabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private System.Windows.Forms.ComboBox SelectProcessDump;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        public MetroFramework.Controls.MetroComboBox SelectProcessEnd;
+        private MetroFramework.Controls.MetroButton endprocess;
+    }
+}
+
